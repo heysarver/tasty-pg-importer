d0fdb52f4cfd35eadbf34b510c1a17e0df1f439c
astromodder <22250203+astromodder@users.noreply.github.com>
2023-06-23 21:27:28 -0400

    optimized

--- import.py
+++ import.py
@@ -1,118 +1,93 @@
 import os
 import sys
-import psycopg2
 import csv
+from psycopg2 import connect
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from dotenv import load_dotenv
 
-# Load .env file if exists
 load_dotenv()
 
-# Get database credentials and csv file name from environment variables and command line arguments
-db_name = os.getenv('DB_NAME')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
-db_table = os.getenv('DB_TABLE')
-csv_file_name = os.getenv('CSV_FILE_NAME')
+db_credentials = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+}
+db_table = os.getenv("DB_TABLE")
+csv_file_name = os.getenv("CSV_FILE_NAME") or (sys.argv[1] if len(sys.argv) > 1 else None)
 
-if len(sys.argv) > 1:
-    csv_file_name = sys.argv[1]
 
-# Function to clean up data
-def clean_data(headers, row, search_replace_dict, default_value_dict):
+def clean_data(headers, row, data_cleaning_dict):
     for i, value in enumerate(row):
-        if headers[i] in search_replace_dict and value == search_replace_dict[headers[i]][0]:
-            row[i] = search_replace_dict[headers[i]][1]
-        elif headers[i] in default_value_dict and value == '':
-            row[i] = default_value_dict[headers[i]]
-        if headers[i] == 'Date':
-            row.append(value[:10])  # Add "trade_day" to the end of the row
+        header = headers[i]
+        if header in data_cleaning_dict:
+            if value == data_cleaning_dict[header]["search"]:
+                row[i] = data_cleaning_dict[header]["replace"]
+            elif value == "" and "default" in data_cleaning_dict[header]:
+                row[i] = data_cleaning_dict[header]["default"]
+
+        if header == "Date":
+            row.append(value[:10])
+
     return row
 
-# Connect to the database
-conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host, port=db_port)
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cursor = conn.cursor()
 
-# Create table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS activity (
-        date date,
-        type text,
-        action text,
-        symbol text,
-        instrument_type text,
-        description text,
-        value numeric DEFAULT 0,
-        quantity numeric DEFAULT 0,
-        average_price numeric DEFAULT 0,
-        commissions numeric DEFAULT 0,
-        fees numeric DEFAULT 0,
-        multiplier numeric DEFAULT 1,
-        root_symbol text,
-        underlying_symbol text,
-        expiration_date text,
-        strike_price text,
-        call_or_put text,
-        order_number text,
-        trade_day date
-    )
-""")
+data_cleaning_dict = {
+    "Date": {"search": "T", "replace": " "},
+    "Commissions": {"search": "--", "replace": "0"},
+    "Value": {"default": "0"},
+    "Quantity": {"default": "0"},
+    "Average Price": {"default": "0"},
+    "Fees": {"default": "0"},
+    "Multiplyer": {"default": "1"},
+}
 
-# Define search and replace values and default values
-search_replace_dict = {
-                        'Date': ['T', ' '],
-                        'Commissions': ['--', '0'],
-                        'column2': ['search_value', 'replace_value']
-                      }
-default_value_dict = {
-                        'Value': '0',
-                        'Quantity': '0',
-                        'Average Price': '0',
-                        'Commissions': '0',
-                        'Fees': '0',
-                        'Multiplyer': '1'
-                      }
+with connect(**db_credentials) as conn:
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-# Load CSV file into the table
-with open(csv_file_name, 'r') as f:
-    reader = csv.reader(f)
-    headers = next(reader)  # Get the header row
-    for row in reader:
-        # Clean up the data
-        row = clean_data(headers, row, search_replace_dict, default_value_dict)
+    with conn.cursor() as cursor:
         cursor.execute(
             f"""
-            INSERT INTO {db_table} (
-                "date",
-                type,
-                action,
-                symbol,
-                instrument_type,
-                description,
-                value,
-                quantity,
-                average_price,
-                commissions,
-                fees,
-                multiplier,
-                root_symbol,
-                underlying_symbol,
-                expiration_date,
-                strike_price,
-                call_or_put,
-                order_number,
-                trade_day
-            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """,
-            row
+            CREATE TABLE IF NOT EXISTS {db_table} (
+                date date,
+                type text,
+                action text,
+                symbol text,
+                instrument_type text,
+                description text,
+                value numeric DEFAULT 0,
+                quantity numeric DEFAULT 0,
+                average_price numeric DEFAULT 0,
+                commissions numeric DEFAULT 0,
+                fees numeric DEFAULT 0,
+                multiplier numeric DEFAULT 1,
+                root_symbol text,
+                underlying_symbol text,
+                expiration_date text,
+                strike_price text,
+                call_or_put text,
+                order_number text,
+                trade_day date
+            )
+            """
         )
 
-# Commit the transaction
-conn.commit()
+        with open(csv_file_name, "r") as f:
+            reader = csv.reader(f)
+            headers = next(reader)
 
-# Close the connection
-cursor.close()
-conn.close()
+            for row in reader:
+                cleaned_row = clean_data(headers, row, data_cleaning_dict)
+
+                cursor.execute(
+                    f"""
+                    INSERT INTO {db_table} (
+                        date, type, action, symbol, instrument_type,
+                        description, value, quantity, average_price,
+                        commissions, fees, multiplier, root_symbol,
+                        underlying_symbol, expiration_date, strike_price,
+                        call_or_put, order_number, trade_day
+                    ) VALUES %s""",
+                    (tuple(cleaned_row),),
+                )
